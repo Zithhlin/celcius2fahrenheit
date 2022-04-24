eb1245eb84903065bd6c004d96bec69d6b71e5bb
Zithhlin <97276464+Zithhlin@users.noreply.github.com>
2022-04-24 10:23:11 +0530

    Add files via upload

---
+++ celcius2fahrenheit.java
@@ -0,0 +1,9 @@
+public class celcius2fahrenheit
+{
+    public static void main (float celcius)
+    {
+        float fahrenheit = 0;
+        fahrenheit = (celcius * 9/5) + 32;
+        System.out.println("Temperature in fahrenheit: "+fahrenheit);
+    }
+}
